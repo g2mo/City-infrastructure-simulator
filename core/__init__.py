86958c7ad00fc96d854d0544a78f5df4fcb1c46e
Guglielmo Cimolai <guglielmo.cimolai@icloud.com>
2025-07-29 23:47:16 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
 """Core city infrastructure models"""
-from .city import City, Building
+from .city import City, Building, DistrictCenter
 from .layout import LayoutGenerator
 from .building_generator import BuildingGenerator
 
-__all__ = ['City', 'Building', 'LayoutGenerator', 'BuildingGenerator']
+__all__ = ['City', 'Building', 'DistrictCenter', 'LayoutGenerator', 'BuildingGenerator']
